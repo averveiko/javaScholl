bfcfcdbf32aa41dbae77fc69fbc3d4706cad8370
Alexander Verveyko <averveiko@gmail.com>
2017-11-11 21:39:28 +0300

    complete ingredientdao

--- lesson24/RecipeStore/src/main/java/ru/sbt/averveyko/App.java
+++ lesson24/RecipeStore/src/main/java/ru/sbt/averveyko/App.java
@@ -5,6 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.sbt.averveyko.dao.IngredientDao;
 import ru.sbt.averveyko.model.Ingredient;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
@@ -17,8 +19,21 @@ public class App {
 //
 //        ingredientDao.insert(ingredient);
 
-        Ingredient ingredient = ingredientDaoImpl.getByPK(2);
-        System.out.println(ingredient);
+//        Ingredient ingredient = ingredientDaoImpl.getByPK(2);
+//        System.out.println(ingredient);
+//
+//        System.out.println("delete ingredient 2");
+//        ingredientDaoImpl.deleteByPK(2);
+
+        Ingredient ingredient = ingredientDaoImpl.getByPK(3);
+        ingredient.setName("Картошечка");
+        ingredientDaoImpl.update(ingredient);
+
+        System.out.println("list all");
+        List<Ingredient> ingredients = ingredientDaoImpl.getAll();
+        for (Ingredient ingredient1 : ingredients) {
+            System.out.println(ingredient1);
+        }
 
     }
 }
--- lesson24/RecipeStore/src/main/java/ru/sbt/averveyko/dao/IngredientDaoImpl.java
+++ lesson24/RecipeStore/src/main/java/ru/sbt/averveyko/dao/IngredientDaoImpl.java
@@ -11,6 +11,7 @@ import ru.sbt.averveyko.dao.utils.IngredientMapper;
 import ru.sbt.averveyko.model.Ingredient;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 
 public class IngredientDaoImpl implements IngredientDao {
@@ -22,6 +23,19 @@ public class IngredientDaoImpl implements IngredientDao {
             "SELECT id, name\n" +
                     "FROM Ingredient\n" +
                     "WHERE id=:id;";
+
+    private static final String QUERY_GET_ALL =
+            "SELECT id, name\n" +
+                    "FROM Ingredient;";
+
+    private static final String QUERY_DELETE_BY_PK =
+            "DELETE FROM Ingredient\n" +
+                    "WHERE id=:id;";
+
+    private static final String QUERY_UPDATE =
+            "UPDATE Ingredient\n" +
+                    "SET name = :name\n" +
+                    "WHERE id = :id;";
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final IngredientMapper ingredientMapper;
@@ -35,21 +49,34 @@ public class IngredientDaoImpl implements IngredientDao {
     public void insert(Ingredient ingredient) {
         SqlParameterSource param = new BeanPropertySqlParameterSource(ingredient);
         KeyHolder keyHolder = new GeneratedKeyHolder();
-
         int result = namedParameterJdbcTemplate.update(QUERY_INSERT, param, keyHolder);
-
         System.out.println("insert " + result + " ingredient: " + ingredient.getName());
     }
 
     @Override
     public Ingredient getByPK(Integer primaryKey) {
         SqlParameterSource param = new MapSqlParameterSource("id", primaryKey);
-
         Ingredient ingredient = namedParameterJdbcTemplate.queryForObject(QUERY_GET_BY_PK, param, ingredientMapper);
         return ingredient;
     }
 
-    public void testJdbc() {
-        System.out.println("test");
+    @Override
+    public List<Ingredient> getAll() {
+        List<Ingredient> ingredients = namedParameterJdbcTemplate.query(QUERY_GET_ALL, ingredientMapper);
+        return ingredients;
+    }
+
+    @Override
+    public void deleteByPK(Integer primaryKey) {
+        SqlParameterSource param = new MapSqlParameterSource("id", primaryKey);
+        int result = namedParameterJdbcTemplate.update(QUERY_DELETE_BY_PK, param);
+        System.out.println("delete " + result + " ingredient ");
+    }
+
+    @Override
+    public void update(Ingredient ingredient) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(ingredient);
+        int result = namedParameterJdbcTemplate.update(QUERY_UPDATE, param);
+        System.out.println("updated " + result + " ingredient " + ingredient.getName());
     }
 }
