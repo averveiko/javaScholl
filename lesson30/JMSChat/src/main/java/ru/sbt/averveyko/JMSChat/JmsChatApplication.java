74109ed22e542212125f96a3bf6b4520df7ff64e
Alexander Verveyko <averveiko@gmail.com>
2017-11-17 15:42:52 +0300

    JMS Chat init

---
+++ lesson30/JMSChat/src/main/java/ru/sbt/averveyko/JMSChat/JmsChatApplication.java
@@ -0,0 +1,12 @@
+package ru.sbt.averveyko.JMSChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsChatApplication.class, args);
+	}
+}
