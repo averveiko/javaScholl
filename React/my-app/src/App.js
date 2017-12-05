a2f5d65714d7492d80ea91d6ae92cba5f14b3eed
Alexander Verveyko <averveiko@gmail.com>
2017-12-05 21:36:40 +0300

    my-app init

---
+++ React/my-app/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Header from "./Header";
+import RegistrationForm from "./RegistrationForm";
+
+const menu = [
+    {
+        link: '/arcticles',
+        label: 'Articles'
+    },
+    {
+        link: '/contacts',
+        label: 'Contacts'
+    },
+    {
+        link: '/posts',
+        label: 'Posts'
+    }
+];
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header items={menu}/>
+
+                <RegistrationForm/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ React/my-app/src/Header.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types'
+
+class Header extends Component {
+    static propTypes = {
+        items: PropTypes.array.isRequired
+    };
+
+    render() {
+        console.log("items: ", this.props.items)
+        return (
+            <div>
+                {this.props.items.map((item, index) =>
+                    <a href={item.link} key={index}>{item.label}</a>
+                )}
+            </div>
+        );
+    }
+}
+
+export default Header;
